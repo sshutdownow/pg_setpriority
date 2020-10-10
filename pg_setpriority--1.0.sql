9693101c2e6ebbba71bfa47873ec4d64ad60b674
Igor <sshutdownow@users.noreply.github.com>
2020-10-10 13:27:55 +0200

    Update pg_setpriority--1.0.sql

--- pg_setpriority--1.0.sql
+++ pg_setpriority--1.0.sql
@@ -1,11 +1,18 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION pg_setpriority" to load this file. \quit
 
-CREATE FUNCTION setpriority(integer) RETURNS integer
+CREATE FUNCTION setpriority(priority integer) RETURNS integer
     AS '$libdir/pg_setpriority', 'pg_setpriority'
+    LANGUAGE C VOLATILE;
+    
+CREATE FUNCTION getpriority(pid integer default 0) RETURNS integer
+    AS '$libdir/pg_setpriority', 'pg_getpriority'
     LANGUAGE C VOLATILE;
 
 ALTER FUNCTION setpriority(integer)
-  OWNER TO postgres;
-  COMMENT ON FUNCTION setpriority(integer) IS 'set the priority of PostgreSQL process';
+  COMMENT ON FUNCTION setpriority(integer) IS 'set the priority of current PostgreSQL process';
+
+ALTER FUNCTION setpriority(integer)
+  COMMENT ON FUNCTION getpriority(integer) IS 'get the priority of PostgreSQL process';
+
   
