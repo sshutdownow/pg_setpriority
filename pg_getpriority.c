4a4213523805670dc81fc4d7336e585504942fe3
Igor <sshutdownow@users.noreply.github.com>
2020-10-10 13:11:50 +0200

    Create pg_getpriority.c

---
+++ pg_getpriority.c
@@ -0,0 +1,33 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <sys/time.h>
+#include <sys/resource.h>
+
+#include "postgres.h"
+#include "fmgr.h"
+
+PG_MODULE_MAGIC;
+
+
+PG_FUNCTION_INFO_V1(pg_getpriority);
+
+extern Datum pg_getpriority(PG_FUNCTION_ARGS); /* no gcc warn */
+
+Datum pg_getpriority(PG_FUNCTION_ARGS)
+{
+	int result, lasterr;
+    int32 priority = PG_GETARG_INT32(0);
+
+    lasterr = errno;
+    /* reset global error variable */
+    errno = 0;
+    result = getpriority(PRIO_PROCESS, 0);
+    if (result == -1 && errno != 0) { /* getpriority() can legitimately return the value -1 */
+        ereport(ERROR, (errcode(ERRCODE_SYSTEM_ERROR), (errmsg("getpriority() failed: %m"))));
+    }
+    errno = lasterr;
+
+	PG_RETURN_INT32(result);
+}
