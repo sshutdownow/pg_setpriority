3a71802f6593486051df776be58961ec1f0a9289
Igor Popov <sshutdownow@users.noreply.github.com>
2025-02-07 14:34:47 +0200

    Update pg_setpriority--1.0.sql

--- pg_setpriority--1.0.sql
+++ pg_setpriority--1.0.sql
@@ -8,11 +8,10 @@ CREATE FUNCTION setpriority(priority integer) RETURNS integer
 CREATE FUNCTION getpriority(pid integer default 0) RETURNS integer
     AS '$libdir/pg_setpriority', 'pg_getpriority'
     LANGUAGE C VOLATILE;
-
+/*
 ALTER FUNCTION setpriority(integer)
   COMMENT ON FUNCTION setpriority(integer) IS 'set the priority of current PostgreSQL process';
 
 ALTER FUNCTION setpriority(integer)
   COMMENT ON FUNCTION getpriority(integer) IS 'get the priority of PostgreSQL process';
-
-  
+*/
